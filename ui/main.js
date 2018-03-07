15c3bcfd0e567fd563056657dfb7066e65b8c5b2
mram98 <36890943+mram98@users.noreply.github.com>
2018-03-07 23:11:38 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,24 @@
-console.log('Loaded!');
+ //Counter code
+ var button = document.getElementById('counter');
+ 
+ button.onclick = function () {
+     
+     //Create a request object
+     var request = new XMLHttpRequest(); 
+     //Capture the response and store it in a variable
+     request.onreadystatechange = function () {
+         if (request.readystate === XMLHttpRequest.DONE){
+             // Take some action
+             if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count'); 
+     span.innerHTML = counter.toString();
+             }
+         }
+         // Not done yet
+     };
+     
+     //Make a request
+     request.open('GET','http://http://mramgopal98.imad.hasura-app.io/counter',true);
+     request.send(null);
+ };
