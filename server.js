18948b547e95fff53bdc832701cdd5996da33969
mram98 <36890943+mram98@users.noreply.github.com>
2018-03-25 12:15:09 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles= {
+var articles = {
  'article-one' :{
     title: 'Article One | M. Ram Gopal',
     heading: 'Article One',
